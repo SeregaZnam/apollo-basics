1a8457c60bed5d07797949981c817f1ae5fd83e5
Sergey_Michkov <michkov@mercurydevelopment.com>
2021-05-20 01:43:17 +0400

    Added adding todo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import './App.css';
 import { useState } from 'react';
 import { useQuery, useMutation } from '@apollo/client';
-import ListTodos from './graphql/ListTodos.query.graphql';
+import ListTodosQuery from './graphql/ListTodos.query.graphql';
 import AddTodoMutation from './graphql/AddTodo.mutation.graphql';
+import EditTodoMutation from './graphql/EditTodo.mutation.graphql';
+import RemoveTodoMutation from './graphql/RemoveTodo.mutation.graphql';
+import produce from 'immer';
 
 function App() {
-  const { loading, data: { todos } = {} } = useQuery(ListTodos);
-  const [addTodo] = useMutation(AddTodoMutation);
+  const { loading, data: { todos } = {} } = useQuery(ListTodosQuery);
+  const [removeTodo] = useMutation(RemoveTodoMutation);
+  const [addTodo] = useMutation(AddTodoMutation, {
+    update(cache, result) {
+      const listTodosQueryResults = cache.readQuery({ query: ListTodosQuery });
+
+      const newListTodosQueryResults = produce(listTodosQueryResults, (draft) => {
+        draft.todos.push(result.data.newTodo.node);
+      });
+
+      cache.writeQuery({
+        query: ListTodosQuery,
+        data: newListTodosQueryResults
+      });
+    }
+  });
+  const [editTodo] = useMutation(EditTodoMutation);
 
   const [newTodoText, setNewTodoText] = useState('');
 
@@ -21,6 +39,29 @@ function App() {
           <li key={t.id}>
             {t.task}
             {t.complete}
+            <button
+              onClick={() => {
+                editTodo({
+                  variables: {
+                    id: t.id,
+                    text: `[MODIFIED ${Math.random()}] ${t.task}`
+                  }
+                });
+              }}
+            >
+              Add modified
+            </button>
+            <button
+              onClick={() => {
+                removeTodo({
+                  variables: {
+                    id: t.id
+                  }
+                });
+              }}
+            >
+              Delete
+            </button>
           </li>
         ))}
       </ul>
